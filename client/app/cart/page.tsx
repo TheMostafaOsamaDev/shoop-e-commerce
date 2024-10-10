bab0119caa400833f8f74bc645ebe63bbff4f63c
Mostafa Osama <mostafaos2021fp@gmail.com>
2024-10-10 08:50:57 +0300

    Finished Checkout layout page

--- client/app/cart/page.tsx
+++ client/app/cart/page.tsx
@@ -16,18 +16,14 @@ export default function CartPage() {
     enabled: !session?.user?.id,
   });
 
-  // /* if(isPending) */ if(true) {
-
-  // }
-
-  console.log(data);
-
   return (
-    <div className="min-h-screen container grid grid-cols-[1fr_auto]">
+    <div className="min-h-screen container grid justify-between gap-5 grid-cols-[1fr_340px]">
       <div>
         <CartItemsTable cartItems={data?.data || []} />
       </div>
-      <div></div>
+      <div>
+        <CheckOutPanel cartItems={data?.data || []} />
+      </div>
     </div>
   );
 }
@@ -45,7 +41,6 @@ import { Cart } from "@/types/cart";
 import Image from "next/image";
 import { getAssetsUrl } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-
 const CartItemsTable = ({ cartItems }: { cartItems: Cart[] }) => {
   return (
     <Table>
@@ -100,7 +95,7 @@ const CartItemsTable = ({ cartItems }: { cartItems: Cart[] }) => {
                 </div>
               </TableCell>
               <TableCell className="text-nowrap font-medium">
-                {p.price} $
+                $ {p.price}
               </TableCell>
             </TableRow>
           );
@@ -113,5 +108,81 @@ const CartItemsTable = ({ cartItems }: { cartItems: Cart[] }) => {
         </TableRow> */}
       </TableBody>
     </Table>
+  );
+};
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+const CheckOutPanel = ({ cartItems }: { cartItems: Cart[] }) => {
+  let totalPrice: number = 0;
+
+  if (cartItems.length) {
+    totalPrice = cartItems.reduce((acc, item) => {
+      return acc + item.product.price * item.quantity;
+    }, 0);
+  }
+
+  return (
+    <div className="sticky top-0 pt-4 flex flex-col gap-6">
+      <Card>
+        <CardHeader className="p-4">
+          <CardTitle className="text-lg">Apply Coupon</CardTitle>
+          <CardDescription className="text-xs font-medium">
+            Use coupons to get discounts on your purchase.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex items-center gap-2 p-4">
+          <Input type="text" placeholder="code" />
+
+          <Button>Apply</Button>
+        </CardContent>
+      </Card>
+
+      <Card className="p-4 space-y-6">
+        <CardHeader className="p-0">
+          <CardTitle className="text-lg">Total</CardTitle>
+        </CardHeader>
+
+        <Separator className="w-full h-[0.8px]" />
+        {/* TODO: Add discount */}
+        <CardContent className="p-0 space-y-3">
+          <div className="font-medium flex items-center justify-between">
+            <span>Total</span>
+
+            <span>$ {totalPrice}</span>
+          </div>
+          <div className="flex items-center justify-between">
+            <span>Delivery</span>
+
+            <span>$ 14.00</span>
+          </div>
+          <div className="flex items-center justify-between">
+            <span>Discount</span>
+
+            <span>-${0}</span>
+          </div>
+        </CardContent>
+
+        <Separator className="w-full h-[0.8px]" />
+
+        <CardFooter className="p-0 flex flex-col space-y-3">
+          <div className="font-medium flex items-center w-full justify-between">
+            <span>Total</span>
+
+            <span>${totalPrice + 14}</span>
+          </div>
+
+          <Button className="w-full">Checkout</Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
